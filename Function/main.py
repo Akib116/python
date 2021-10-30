c3453d9f7a3af2ef8f462652764b3e87d49957be
Akib116 <17-35708-3@student.aiub.edu>
2021-10-30 12:37:53 +0600

    add function folder

---
+++ Function/main.py
@@ -0,0 +1,45 @@
+# def add(x, y):
+#     sum = x + y
+#     print(sum)
+#
+#
+# def message():
+#     print("No Perameter")
+#
+#
+# add(50, 30)
+# message()
+
+# round and abs built in function
+
+# a = 3.5
+# b = 4.5
+# c = -5
+# d = -6j
+# print(round(a))
+# print(round(b))
+# print(round(a, 1))
+# print(abs(c))
+# print(abs(d))
+
+# function argument
+
+# def add_numbers(n1, n2):
+#     result = n1 + n2
+#     print("The sum is", result)
+#
+#
+# add_numbers(5.6, 6.7)
+
+# function to find average marks
+
+# def find_average_marks(marks):
+#     sum_marks = sum(marks)
+#     total_subject = len(marks)
+#     average_marks = sum_marks / total_subject
+#     return average_marks
+#
+#
+# marks = [55, 64, 75, 80, 65]
+# average_marks= find_average_marks(marks)
+# print("average marks:", average_marks)
